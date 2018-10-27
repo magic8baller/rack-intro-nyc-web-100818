6a98c8db55a6228843c10622938dafdc1db35416
magic8baller <lturis@gmail.com>
2018-10-27 15:22:49 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is IS IT ME YOU'RE LOOKING FOR?"
     resp.finish
   end
 
